11241769765ef4df8c927d7487e4be14829eca61
Xuemin Guan <Xuemin Guan>
2019-05-13 12:53:19 +0100

    :tada: First commit. This is a bare bone Typescript project which has a minium set up

---
+++ test/foo.test.ts
@@ -0,0 +1,11 @@
+import { sum } from '../src/foo';
+
+test('basic', () => {
+    expect(sum(1,2)).toBe(3);
+});
+
+describe("The class under test", () => {
+    it("should work", () => {
+        expect(1).toBe(1);
+    });
+});
