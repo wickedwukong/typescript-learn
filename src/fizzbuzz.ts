ed5c4e2bb93cdd74c8955f35a4887ac2095c43cf
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-14 11:07:33 +0100

    return Fizz for multiples of three, Buzz for multiples of five

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,2 +1,8 @@
 export const fizzbuzz
-    = (): any[] => [...Array(100).keys()].map(i => i + 1);
+    = (): any[] => {
+    return [...Array(100).keys()]
+        .map(i => i + 1)
+        .map(i => {
+            return (i % 3 == 0) ? 'Fizz' : (i % 5 == 0) ? 'Buzz' : i;
+        });
+};
--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -5,7 +5,16 @@ describe("fizzbuzz", () => {
         expect(fizzbuzz().length).toEqual(100);
     });
 
-    it("give 1 for 1st value", () => {
+    it("give 1 for 1st value, and 2 for 2nd value", () => {
         expect(fizzbuzz()[0]).toEqual(1);
+        expect(fizzbuzz()[1]).toEqual(2);
+    });
+
+    it("give Fizz for 3rd value", () => {
+        expect(fizzbuzz()[2]).toEqual('Fizz');
+    });
+
+    it("give Buzz for 5th value", () => {
+        expect(fizzbuzz()[4]).toEqual('Buzz');
     });
 });
