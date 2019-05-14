b9719bf5abed7e19f7a98369879f7ebd12368a83
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-14 11:28:53 +0100

    use union type as the return type so that we can don't have to use the more generic type any

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,12 +1,13 @@
-export const fizzbuzz = (): any[] =>
+export const fizzbuzz = (): (string | number)[] =>
     number1To100().map(i => transformToFizzBuzz(i));
 
-function number1To100() {
+function number1To100(): number[] {
     return [...Array(100).keys()]
         .map(i => i + 1);
 }
 
-function transformToFizzBuzz(i: number) {
+function transformToFizzBuzz(i: number): string | number {
     return (i % 3 == 0 && i % 5 == 0) ? 'FizzBuzz' : (i % 3 == 0) ? 'Fizz' : (i % 5 == 0) ? 'Buzz' : i;
 }
+
 
