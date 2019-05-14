b36f96ed6e30c6ce52422c67441c08c77bfe7426
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-14 11:22:44 +0100

    completed fizz buzz functional requirements

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,7 +1,3 @@
-function transformToFizzBuzz(i: number) {
-    return (i % 3 == 0) ? 'Fizz' : (i % 5 == 0) ? 'Buzz' : i;
-}
-
 export const fizzbuzz = (): any[] =>
     number1To100().map(i => transformToFizzBuzz(i));
 
@@ -9,3 +5,8 @@ function number1To100() {
     return [...Array(100).keys()]
         .map(i => i + 1);
 }
+
+function transformToFizzBuzz(i: number) {
+    return (i % 3 == 0 && i % 5 == 0) ? 'FizzBuzz' : (i % 3 == 0) ? 'Fizz' : (i % 5 == 0) ? 'Buzz' : i;
+}
+
--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -10,11 +10,18 @@ describe("fizzbuzz", () => {
         expect(fizzbuzz()[1]).toEqual(2);
     });
 
-    it("give Fizz for 3rd value", () => {
+    it("give Fizz for 3rd and 99th value", () => {
         expect(fizzbuzz()[2]).toEqual('Fizz');
+        expect(fizzbuzz()[98]).toEqual('Fizz');
     });
 
-    it("give Buzz for 5th value", () => {
+    it("give Buzz for 5th value and 100th value", () => {
         expect(fizzbuzz()[4]).toEqual('Buzz');
+        expect(fizzbuzz()[99]).toEqual('Buzz');
+    });
+
+    it("give FizzBuzz for 15th and 90th value", () => {
+        expect(fizzbuzz()[14]).toEqual('FizzBuzz');
+        expect(fizzbuzz()[89]).toEqual('FizzBuzz');
     });
 });
